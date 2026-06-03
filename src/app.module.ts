5c3c263c9f21f4b0779bb125a3ffe0e303f1486b
Alex <alexandru.bindiu@stud.ubbcluj.ro>
2026-06-03 16:03:43 +0300

    modified:   src/app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -36,7 +36,7 @@ import { ChatModule } from './chat/chat.module';
     GeneratorModule,
     OrdersModule,
     MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/dulcestoc'),
-    //ChatModule,
+    ChatModule,
   ],
 })
 export class AppModule {}
